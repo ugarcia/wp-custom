ac80be1e246d8c3e15eb4d783dc9acab8abcd547
unai <unai.gg77@gmail.com>
2015-10-18 16:14:55 +0200

    Creating plugin ...

--- wp-content/plugins/cp-portfolio/cp-portfolio.php
+++ wp-content/plugins/cp-portfolio/cp-portfolio.php
@@ -3,12 +3,40 @@
 Plugin Name: Custom Portfolio
 Plugin URI: https://github.com/ugarcia/wp-custom.git
 Description: Custom Portfolio Data Type.
-Version: 0.0.1
+Version: 1.0.0
 Author: Frontcoder
 Author URI: http://frontcoder.com/
 Text Domain: cp-portfolio
 License: GPLv2
+Copyright 2015 Unai Garcia (email : unai.gg77@gmail.com)
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+156 ❘ CHAPTER 8 PLUGIN DEVELOPMENT
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 */
 
-define('CUSTOM_PORFOLIO_VERSION', '0.0.1');
-define('CUSTOM_PORFOLIO_HOME', dirname(__FILE__));
+
+function cp_portfolio_install() {
+
+	global $wp_version;
+
+	if ( version_compare( $wp_version, '4.1', '<' ) ) {
+		wp_die( 'This plugin requires WordPress version 4.1 or higher.' );
+	}
+}
+register_activation_hook( __FILE__, 'cp_portfolio_install' );
+
+
+function cp_portfolio_deactivate() {
+
+}
+register_deactivation_hook( __FILE__, 'cp_portfolio_deactivate' );
+
